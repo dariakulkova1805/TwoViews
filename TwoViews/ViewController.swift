b16f83e53dae2ab17f54c526717ded674cf70fd6
Daria Kulkova <d.kulkovva@icloud.com>
2022-11-30 15:31:39 +0300

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by DARYA on 11/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
